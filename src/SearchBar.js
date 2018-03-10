a4645f3bb08622a7f893fdbce72b602bfd5d6193
JerinBoyd <maxers@mac.com>
2018-03-09 19:38:41 -0500

    implemented search reducer

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 const SearchBar = props => {
-    return (
-        <div>
-            <input type="text"
-            value={props.searchTerm} 
-            onChange={(e) => props.updateSearch(e.target.value)} />
-        </div>
-    );
-}
-
+  return (
+    <div>
+      <input
+        type="text"
+        value={props.searchTerm}
+        onChange={e => props.updateSearch(e.target.value)}
+      />
+    </div>
+  );
+};
 
 const mapStatetoProps = state => {
-    return {
-  searchTerm: state.search 
-}
-}
+  return {
+    searchTerm: state.search
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        updateSearch: term => dispatch({type: 'UPDATE_SEARCH', payload: {value:term}})
-    };
+  return {
+    updateSearch: term =>
+      dispatch({ type: "UPDATE_SEARCH", payload: { value: term } })
+  };
 };
 
 export default connect(mapStatetoProps, mapDispatchToProps)(SearchBar);
