80c1c183f047295bdbf1c78db51c0210e6320f75
JerinBoyd <maxers@mac.com>
2018-03-12 18:18:02 -0400

    renamed key to id a

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
 import ProductList from "./ProductList";
-import SearchBar from './SearchBar';
+import SearchBar from "./SearchBar";
 
 class App extends Component {
   render() {
@@ -23,5 +23,6 @@ const mapDispatchToProps = dispatch => {
     dispatch: dispatch
   };
 };
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
